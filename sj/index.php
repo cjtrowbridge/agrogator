b882b8fe08c5fa489c25285f49927cf56c39a118
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-05 16:37:23 -0700

    Create index.php
    created sj subtopic with a few subreddits

---
+++ sj/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags always come first -->
+		<meta charset="utf-8">
+		<title>SJ | Agrogator</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<script src="https://www.google.com/jsapi"></script>
+		<script src="/js/agrogator-1.0.js"></script>
+		<link rel="stylesheet" href="/css/default-theme.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="agrogator">
+						<h2>
+							<i class="material-icons agrogate_refresh" onclick="Agrogate();">cached</i>
+							SJ
+						</h2>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/feminism/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/AskFeminists/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/domesticviolence/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/transgender/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/MensLib/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/BodyAcceptance/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/SexPositive/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/Meta_Feminism/.rss"></span>
+						<span class="agrogator_enclosure" data-uri="https://www.reddit.com/r/BodyAcceptance/.rss"></span>
+						<div class="agrogator_container"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- jQuery first, then Bootstrap JS. -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+	</body>
+</html>
